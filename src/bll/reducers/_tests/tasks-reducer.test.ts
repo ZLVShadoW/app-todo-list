79dec9cb828f9f5ce76299b1fe9a498ee9d8d0e9
ZLVShadoW <zlvshadow@gmail.com>
2022-08-05 07:08:55 +0300

    update: tasksReducer test

--- src/bll/reducers/_tests/tasks-reducer.test.ts
+++ src/bll/reducers/_tests/tasks-reducer.test.ts
@@ -1,5 +1,5 @@
 import {TasksInitStateType} from '../../types/tasks-type';
-import {addTask, setTasks} from '../../actions/tasks-actions';
+import {addTask, removeTask, setTasks} from '../../actions/tasks-actions';
 import {TaskStatuses, TaskType} from '../../../types/types';
 import {tasksReducer} from '../tasks-reducer';
 
@@ -7,7 +7,66 @@ let startState: TasksInitStateType
 
 beforeEach(() => {
     startState = {
-        '1': []
+        '1': [{
+            todoListId: '1',
+            id: '1',
+            title: 'task 1 toto list 1',
+            status: TaskStatuses.New,
+            startDate: '',
+            deadline: '',
+            priority: 0,
+            description: '',
+            addedDate: '',
+            order: 1
+        },
+            {
+                todoListId: '1',
+                id: '2',
+                title: 'task 2 toto list 1',
+                status: TaskStatuses.New,
+                startDate: '',
+                deadline: '',
+                priority: 0,
+                description: '',
+                addedDate: '',
+                order: 1
+            }],
+        '2': [{
+            todoListId: '2',
+            id: '1',
+            title: 'task 1 toto list 2',
+            status: TaskStatuses.New,
+            startDate: '',
+            deadline: '',
+            priority: 0,
+            description: '',
+            addedDate: '',
+            order: 1
+        },
+            {
+                todoListId: '2',
+                id: '2',
+                title: 'task 2 toto list 2',
+                status: TaskStatuses.New,
+                startDate: '',
+                deadline: '',
+                priority: 0,
+                description: '',
+                addedDate: '',
+                order: 1
+            },
+            {
+                todoListId: '2',
+                id: '3',
+                title: 'task 3 toto list 2',
+                status: TaskStatuses.New,
+                startDate: '',
+                deadline: '',
+                priority: 0,
+                description: '',
+                addedDate: '',
+                order: 1
+            }],
     }
 })
 
@@ -46,19 +105,29 @@ test('tasks should be added to todoList', () => {
 
 test('task should be added to todoList', () => {
     const task: TaskType = {
-            todoListId: '1',
-            id: '2',
-            title: 'task 1',
-            status: TaskStatuses.New,
-            startDate: '',
-            deadline: '',
-            priority: 0,
-            description: '',
-            addedDate: '',
-            order: 1
-        }
+        todoListId: '1',
+        id: '3',
+        title: 'task 1',
+        status: TaskStatuses.New,
+        startDate: '',
+        deadline: '',
+        priority: 0,
+        description: '',
+        addedDate: '',
+        order: 1
+    }
     const endState = tasksReducer(startState, addTask(task))
 
-    expect(endState['1'].length).toBe(1)
+    expect(endState['1'].length).toBe(3)
     expect(endState['1'][0].title).toBe('task 1')
+    expect(endState['1'][2].title).toBe('task 2 toto list 1')
+})
+
+test('task should be removed from todoList', () => {
+
+    const endState = tasksReducer(startState, removeTask('2', '1'))
+
+    expect(endState['2'].length).toBe(2)
+    expect(endState['2'][0].id).toBe('2')
+    expect(endState['2'].every(el => el.id !== '1')).toBeTruthy()
 })
